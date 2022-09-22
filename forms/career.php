88436e084fdbad01b00d5f27fe34aa9b222d923f
VinaySavla <savlavinay77@gmail.com>
2022-09-23 00:11:28 +0530

    Career form

---
+++ forms/career.php
@@ -0,0 +1,41 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require_once __DIR__ . '/phpmailer/src/Exception.php';
+require_once __DIR__ . '/phpmailer/src/PHPMailer.php';
+require_once __DIR__ . '/phpmailer/src/SMTP.php';
+
+// passing true in constructor enables exceptions in PHPMailer
+$mail = new PHPMailer(true);
+
+try {
+    // Server settings
+    // $mail->SMTPDebug = SMTP::DEBUG_SERVER; // for detailed debug output
+    $mail->isSMTP();
+    $mail->Host = 'smtp.office365.com';
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
+
+    $mail->Username = 'legalnotice@sc-legal.in'; // YOUR gmail email
+    $mail->Password = 'hpffgknhmxmbyjnx'; // YOUR gmail password
+
+    // Sender and recipient settings
+    $mail->setFrom('legalnotice@sc-legal.in', 'SC Legal');
+    $mail->addAddress('savlavinay022@gmail.com', 'Vinay Savla');
+    $mail->addReplyTo('legalnotice@sc-legal.in', 'SC Legal'); // to set the reply to
+
+    // Setting the email content
+    $mail->IsHTML(true);
+    $mail->Subject = $_POST['subject'];
+    $mail->Body = 'Client Enquiry: <br>' .'Name ' . $_POST['name'] . '<br>Email: ' . $_POST['email'] . '<br>Phone Number: ' . $_POST['phone'] . '<br>Resume Link: ' . $_POST['resume'] . '<br>Refrence: ' . $_POST['ref'];
+    // $mail->AltBody = $_POST['name'] . $_POST['email'] . $_POST['message'];
+
+    $mail->send();
+    echo "OK";
+} catch (Exception $e) {
+    echo "Error in sending email. Mailer Error: {$mail->ErrorInfo}";
+}
+?>
